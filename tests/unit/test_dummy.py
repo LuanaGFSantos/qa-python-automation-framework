5c0b173a36b65ec6624bec61e635a620a3ef0699
Lú <87151810+LuanaGFSantos@users.noreply.github.com>
2026-02-23 21:14:16 -0300

    test: add unit tests and fix coverage collection

--- tests/unit/test_dummy.py
+++ tests/unit/test_dummy.py
@@ -1,2 +1,8 @@
-def test_dummy():
-    assert True
+from app import app
+
+def test_app_is_created():
+    assert app is not None
+
+def test_route_home_exists():
+    routes = [rule.rule for rule in app.url_map.iter_rules()]
+    assert "/" in routes
